d1713c86be519613f4eaf43a1e6d028ecff5ab74
turtle <hypoturtle@gmail.com>
2024-07-20 20:37:39 +0100

    add todo methods to database

--- app/features/database.py
+++ app/features/database.py
@@ -8,28 +8,70 @@ class Database(Extension):
 
     async def async_start(self) -> None:
         """Connect to db as bot loops starts."""
-        self.bot.db = await aiosqlite.connect("./ee.db")
-        await self.populate_db()
+        self.bot.db = self
+        self.bot.db_conn = await aiosqlite.connect("./ee.db")
+        await self.populate_tables()
 
-    async def populate_db(self) -> None:
-        """Creation of db table."""
-        async with self.bot.db.cursor() as cursor:
-            await cursor.execute("""
-            CREATE TABLE IF NOT EXISTS
-            todo(user_id INTEGER PRIMARY KEY, tasks BLOB)
-            """)
-        await self.bot.db.commit()
+    async def populate_tables(self) -> None:
+        """Run the coroutines to create the db tables."""
+        await self.todo_table()
 
     @listen(events.Connect)
     async def bot_connect(self, event: events.Connect) -> None:
         """Reconnect to db when the bot reconnects."""
         print(event)
-        if not self.bot.db:
-            self.bot.db = await aiosqlite.connect("./ee.db")
+        if not self.bot.db_conn:
+            self.bot.db_conn = await aiosqlite.connect("./ee.db")
 
     @listen(events.Disconnect)
     async def bot_disconnect(self, event: events.Disconnect) -> None:
         """Disconnect the db when the bot disconnects."""
         print(event)
-        await self.bot.db.close()
+        await self.bot.db_conn.close()
         self.bot.db = None
+
+    # TO-DO methods
+
+    async def todo_table(self) -> None:
+        """Creation of to-do db table."""
+        async with self.bot.db_conn.cursor() as cursor:
+            await cursor.execute("""
+            CREATE TABLE IF NOT EXISTS
+            todo (item_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL,
+            category TEXT NULL,
+            item TEXT NOT NULL)
+            """)
+        await self.bot.db_conn.commit()
+
+    async def todo_add(self, user_id: int, item: str, category: str | None = None) -> None:
+        """Add item to to-do table."""
+        async with self.bot.db_conn.cursor() as cursor:
+            query = """INSERT INTO todo (user_id, category, item) VALUES (?,?,?)"""
+            await cursor.execute(query, (user_id, category, item))
+        await self.bot.db_conn.commit()
+
+    async def todo_remove(self, user_id: int, item_id: int) -> bool:
+        """Remove item from to-do table."""
+        async with self.bot.db_conn.cursor() as cursor:
+            query = """DELETE from todo WHERE item_id = ? AND user_id = ?"""
+            try:
+                await cursor.execute(query, (item_id, user_id))
+                await self.bot.db_conn.commit()
+            except aiosqlite.OperationalError:
+                return False
+        return True
+
+    async def todo_listall(self, user_id: int, category: str | None = None) -> list[tuple[str]]:
+        """Fetch all the users items from the db."""
+        async with self.bot.db_conn.cursor() as cursor:
+            query = """SELECT item FROM todo WHERE user_id = ? AND category = ?"""
+            response = await cursor.execute(query, (user_id, category))
+            return response.fetchall()
+
+    async def todo_get_item(self, user_id: int, item_id: int) -> tuple[str]:
+        """Fetch individual item from db."""
+        async with self.bot.db_conn.cursor() as cursor:
+            query = """SELECT * from todo WHERE user_id = ? AND item_id = ?"""
+            response = await cursor.execute(query, (user_id, item_id))
+            return response.fetchone()
