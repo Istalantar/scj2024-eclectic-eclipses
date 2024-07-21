3ef0e854d189851aed950c69239b91b8fddfb0ef
Istalantar <istalantar@gmail.com>
2024-07-21 12:16:48 +0200

    add database interaction

--- app/features/todo_list.py
+++ app/features/todo_list.py
@@ -1,5 +1,3 @@
-from typing import ClassVar
-
 from interactions import (
     ContextMenuContext,
     Extension,
@@ -15,23 +13,20 @@ from interactions import (
 class TodoList(Extension):
     """Todo list extension."""
 
-    todos: ClassVar = []
-
     @slash_command(name="todo", description="Todo list", sub_cmd_name="list", sub_cmd_description="Lists all todos")
     async def todo_list(self, ctx: SlashContext) -> None:
         """List all saved todos as a numbered list."""
-        # TODO: add database interaction
-        if len(self.todos) == 0:
+        todos = await ctx.bot.db.todo_listall(user_id=ctx.author.id)
+        if len(todos) == 0:
             await ctx.send("There are no saved todos.", ephemeral=True)
         else:
-            await ctx.send("\n".join([f"{i}. {item}" for i, item in enumerate(self.todos, start=1)]), ephemeral=True)
+            await ctx.send("\n".join([f"{i}. {item[0]}" for i, item in enumerate(todos, start=1)]), ephemeral=True)
 
     @slash_command(name="todo", description="Todo list", sub_cmd_name="add", sub_cmd_description="Adds a todo")
     @slash_option(name="todo_msg", description="The ToDo you want to add", required=True, opt_type=OptionType.STRING)
     async def todo_add(self, ctx: SlashContext, todo_msg: str) -> None:
         """Add a new todo."""
-        # TODO: add database interaction
-        self.todos.append(todo_msg)
+        await ctx.bot.db.todo_add(user_id=ctx.user.id, item=todo_msg)
         await ctx.send(f"``{todo_msg}`` added to todo list.", ephemeral=True)
 
     @slash_command(name="todo", description="Todo list", sub_cmd_name="remove", sub_cmd_description="Removes a todo")
@@ -43,17 +38,17 @@ class TodoList(Extension):
     )
     async def todo_remove(self, ctx: SlashContext, todo_id: int) -> None:
         """Remove a todo."""
-        # TODO: add database interaction
-        await ctx.send(f"You removed ``{self.todos.pop(todo_id - 1)}`` from your todo list.", ephemeral=True)
+        _, _, _, todo_item = await ctx.bot.db.todo_get_item(user_id=ctx.author.id, item_id=todo_id)
+        await ctx.bot.db.todo_remove(user_id=ctx.author.id, item_id=todo_id)
+        await ctx.send(f"You removed ``{todo_item}`` from your todo list.", ephemeral=True)
 
     @message_context_menu(name="add to todo")
     async def todo_msg_context_add(self, ctx: ContextMenuContext) -> None:
         """Add todo via message context menu."""
-        # TODO: add database interaction
         message: Message = ctx.target
         todo_msg = message.content
         if "\n" in todo_msg:
             await ctx.send("Only single line messages can be added as todo.", ephemeral=True)
         else:
-            self.todos.append(todo_msg)
+            await ctx.bot.db.todo_add(user_id=ctx.author.id, item=todo_msg)
             await ctx.send(f"``{todo_msg}`` added to todo list.", ephemeral=True)
